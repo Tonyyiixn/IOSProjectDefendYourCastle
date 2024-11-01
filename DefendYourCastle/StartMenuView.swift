c4b8d2f4ada130c3bde84aa67a52786fa1ef65da
YixinChen <Tonyyiixn@gmail.com>
2024-11-01 13:17:01 -0400

    Initial Commit

---
+++ DefendYourCastle/StartMenuView.swift
@@ -0,0 +1,60 @@
+//
+//  StartMenuView.swift
+//  DefendYourCastle
+//
+//  Created by Yixin Chen on 10/17/24.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        VStack {
+            Text("Defend Your Castle")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: startGame) {
+                Text("Start Game")
+                    .font(.title)
+                    .frame(width: 200,height: 50)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Button(action: openSettings) {
+                Text("Settings")
+                    .font(.title)
+                    .frame(width: 200,height: 50)
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Button(action: exitGame) {
+                Text("Exit")
+                    .font(.title)
+                    .frame(width: 200,height: 50)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+    
+    func startGame() {
+        // Start game logic
+    }
+    
+    func openSettings() {
+        // Navigate to settings
+    }
+    
+    func exitGame() {
+        // Handle exit logic
+    }
+}
